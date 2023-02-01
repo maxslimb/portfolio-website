68c18ae4cb2cd55298c33d4fe4360acdea98861d
Kishan Patel <kpatel3032@gmail.com>
2023-02-01 14:19:11 -0600

    added mailgun

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,19 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
-
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+require 'vendor/autoload.php';
+use Mailgun\Mailgun;
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+# Instantiate the client.
+$mgClient = Mailgun::create('1b30c3fa29fac233a349e6dad156f4d3-75cd784d-22bfec70', 'https://API_HOSTNAME');
+$domain = "kishanpatel.tech";
+$params = array(
+  'from'    => 'mailgun@kishanpatel.tech',
+  'to'      => 'kpatel3032@gmail.com',
+  'subject' => $_POST['name'] + $_POST['email'] + $_POST['subject'],
+  'text'    => $_POST['message']
+);
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+# Make the call to the client.
+$mgClient->messages()->send($domain, $params);
+ 
 ?>
